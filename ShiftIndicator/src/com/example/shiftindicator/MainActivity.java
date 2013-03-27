2d72cf47a816cf7902f19e88735aa8b48f1096bd
Zac Nelson <znelson1@ford.com>
2013-03-27 15:30:22 -0400

    switch to Figo code.

--- ShiftIndicator/src/com/example/shiftindicator/MainActivity.java
+++ ShiftIndicator/src/com/example/shiftindicator/MainActivity.java
@@ -86,20 +86,20 @@ public class MainActivity extends Activity {
 	boolean justShifted;
 	
 //	FIGO RATIOS rpm/speed
-//	private int ratio1 = 140;
-//	private int ratio2 = 75;
-//	private int ratio3 = 50;
-//	private int ratio4 = 37;
-//	private int ratio5 = 30;
-//	private int ratio6 = 1; // does not exist in Figo
+	private int ratio1 = 140;
+	private int ratio2 = 75;
+	private int ratio3 = 50;
+	private int ratio4 = 37;
+	private int ratio5 = 30;
+	private int ratio6 = 1; // does not exist in Figo
 	
 //	Focus ST RATIOS rpm/speed:
-	private int ratio1 = 114;
-	private int ratio2 = 69;
-	private int ratio3 = 46;
-	private int ratio4 = 36;
-	private int ratio5 = 28;
-	private int ratio6 = 23;
+//	private int ratio1 = 114;
+//	private int ratio2 = 69;
+//	private int ratio3 = 46;
+//	private int ratio4 = 36;
+//	private int ratio5 = 28;
+//	private int ratio6 = 23;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -319,23 +319,23 @@ public class MainActivity extends Activity {
 		    
 		    else next_rpm=min_rpm;
 		    
-//		    if (next_rpm < vehicle_speed*next_ratio){
-//		    	
-//		    	if (!justShifted){
-//        			send2Arduino('['+"1"+']');
-//        			MainActivity.this.runOnUiThread(new Runnable() {
-//    			        public void run() {
-//    			            mShiftCalc.setText("Shift!!");
-//    			        }
-//    			    });
-//        		}
-//		    	
-//        		justShifted = true;
-//		    	 
-//		    	shiftTime = new Date().getTime();
-//		    }
-//		    
-//		    else cancelShift(currentTime);
+		    if (next_rpm < vehicle_speed*next_ratio){
+		    	
+		    	if (!justShifted){
+        			send2Arduino('['+"1"+']');
+        			MainActivity.this.runOnUiThread(new Runnable() {
+    			        public void run() {
+    			            mShiftCalc.setText("Shift!!");
+    			        }
+    			    });
+        		}
+		    	
+        		justShifted = true;
+		    	 
+		    	shiftTime = new Date().getTime();
+		    }
+		    
+		    else cancelShift(currentTime);
 		}
 
 
