fee0322d519cfd6171762c616ac6c0ab481914c7
Zac Nelson <znelson1@ford.com>
2013-04-19 16:54:00 -0400

    begin adding settings to settings class.

--- ShiftIndicator/src/com/example/shiftindicator/SettingsActivity.java
+++ ShiftIndicator/src/com/example/shiftindicator/SettingsActivity.java
@@ -2,12 +2,41 @@ package com.example.shiftindicator;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements
+				OnClickListener {
+	
+	Switch modeSwitch;
+	CheckBox audioCB;
+	CheckBox vibrateCB;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
  
+        modeSwitch = (Switch) findViewById(R.id.pref_operation_mode_key);
     }
+	
+    private void loadPrefs() {
+    	
+    }
+
+    private void savePrefs(String key, boolean value) {
+
+    }
+
+    private void savePrefs(String key, String value) {
+
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
