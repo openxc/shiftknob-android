df085b8abd36f4b5e54e8a2b0b8519f5a7de3beb
Zac Nelson <znelson1@ford.com>
2013-07-24 08:39:54 -0400

    rework app settings. add Peformance Shift Point.

--- src/com/openxc/shiftindicator/SettingsActivity.java
+++ src/com/openxc/shiftindicator/SettingsActivity.java
@@ -1,13 +1,50 @@
 package com.openxc.shiftindicator;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements 
+            SharedPreferences.OnSharedPreferenceChangeListener{
 
+    public static final String KEY_PREF_CALCULATION_MODE = "pref_operation_mode";
+    public static final String KEY_PREF_SHIFT_POINT = "pref_shift_point";
+    public static final String KEY_PREF_CALCULATION = "pref_calculation_mode";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPref, String key) {
+        // TODO Auto-generated method stub
+        if (key.equals(KEY_PREF_CALCULATION_MODE)) {
+            Preference shiftPointPref = findPreference(KEY_PREF_SHIFT_POINT);
+            Preference algorithmPref = findPreference(KEY_PREF_CALCULATION);
+            if (sharedPref.getBoolean(key, false)) {
+                shiftPointPref.setEnabled(true);
+                algorithmPref.setEnabled(false);
+            } else {
+                shiftPointPref.setEnabled(false);
+                algorithmPref.setEnabled(true);
+            }
+        }
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
